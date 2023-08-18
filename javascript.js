62fa66e504aea2e89155152b316551575bb27f9d
Mitchell <72991363+1q2a1@users.noreply.github.com>
2023-08-18 22:54:15 +0800

    Added change grid size functionality
    click button and enter prompt to change grid size

--- javascript.js
+++ javascript.js
@@ -1,16 +1,34 @@
 // create grid
-let container = document.querySelector(".container")
-for (i=0; i<16; i++){
-    let newRow = document.createElement("div")
-    newRow.className = "row"
-    for (j=0; j<16; j++){
-        let newDiv = document.createElement("div")
-        newDiv.className = "cell"
-        newDiv.style.border = "1px solid black"
-        newDiv.style.width = '40px'
-        newDiv.style.height = '40px'
-        newRow.appendChild(newDiv)
+function createGrid(size){
+    let body = document.querySelector("body")
+    let container = document.createElement("div")
+    container.className = "container"
+
+    for (i=0; i<size; i++){
+        let newRow = document.createElement("div")
+        newRow.className = "row"
+        for (j=0; j<size; j++){
+            let newDiv = document.createElement("div")
+            newDiv.className = "cell"
+            newDiv.style.border = "1px solid black"
+            newDiv.style.width = '40px'
+            newDiv.style.height = '40px'
+            newRow.appendChild(newDiv)
+        }
+        container.append(newRow)
     }
-    container.append(newRow)
+    body.appendChild(container)
 }
 
+function changeGridSize(e){
+    console.log("hi")
+    const gridSize = prompt("Enter Grid Size")
+    let container = document.querySelector(".container")
+    container.remove()
+    createGrid(gridSize)
+}
+
+const gridBtn = document.querySelector("button")
+gridBtn.addEventListener("click", changeGridSize)
+
+createGrid(2)
