f2712b3db3c1b5fa3b051a91e4c252470ce38932
Mitchell <72991363+1q2a1@users.noreply.github.com>
2023-08-19 15:16:04 +0800

    Modified grid creation logic
    ensure grid is always a square and that cell size changes according to gridsize.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,33 @@
-// create grid
-function createGrid(size){
-    let body = document.querySelector("body")
+function createContainer(){
     let container = document.createElement("div")
     container.className = "container"
 
+    let gridSection = document.querySelector(".grid-section")
+    let rect = gridSection.getBoundingClientRect()
+    let containerWidth = rect.width
+    let containerHeight = rect.height
+    let containerSize = 0.95* Math.min(containerWidth, containerHeight)
+
+    let style = document.createElement("style")
+    style.innerHTML = `
+        .container{
+            width: ${containerSize}px;
+            height:${containerSize}px;
+        }
+        `
+    
+    document.head.appendChild(style)
+    gridSection.appendChild(container)
+}
+
+
+function createGrid(size){
+    let oldContainer = document.querySelector(".container")
+    oldContainer.remove()
+
+    createContainer()
+    let newContainer = document.querySelector(".container")
+    let cellSize = (newContainer.getBoundingClientRect().width)/size
     for (i=0; i<size; i++){
         let newRow = document.createElement("div")
         newRow.className = "row"
@@ -11,24 +35,26 @@ function createGrid(size){
             let newDiv = document.createElement("div")
             newDiv.className = "cell"
             newDiv.style.border = "1px solid black"
-            newDiv.style.width = '40px'
-            newDiv.style.height = '40px'
+            newDiv.style.width = cellSize + "px"
+            newDiv.style.height = cellSize + "px"
             newRow.appendChild(newDiv)
         }
-        container.append(newRow)
+        newContainer.append(newRow)
     }
-    body.appendChild(container)
+    
+    let gridSection = document.querySelector(".grid-section")
+    gridSection.appendChild(newContainer)
 }
 
+
+
 function changeGridSize(e){
-    console.log("hi")
     const gridSize = prompt("Enter Grid Size")
-    let container = document.querySelector(".container")
-    container.remove()
     createGrid(gridSize)
 }
 
 const gridBtn = document.querySelector("button")
 gridBtn.addEventListener("click", changeGridSize)
 
-createGrid(2)
+// createContainer()
+createGrid(5)
